440dacc80751243495a81193c3d3236d49fb4dbf
Otavio Marques <marquesotavio251204@gmail.com>
2024-11-08 14:15:33 -0300

    subindo projeto

---
+++ src/test/java/br/com/otavio/educational/EducationalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.otavio.educational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
